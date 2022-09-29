d1a5091127e27d7c7b28dcf3ed95af8de24e3e47
Saurav903 <sk8517167@gmail.com>
2022-09-29 15:07:00 +0530

    final of index done

--- indexscript.js
+++ indexscript.js
@@ -50,6 +50,44 @@ arr1.forEach(function(e){
     div1.append(div2,div3);
     below_nav4.append(div1);
 });
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = document.querySelector('.carousel').children;
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener("click", () => {
+    nextImage('next');
+});
+next.addEventListener('click', () => {
+    nextImage('prev');
+});
+
+function nextImage(direction) {
+    if(direction=="next") {
+        index++;
+        if(index==totalImages) {
+            index = 0;
+        }
+    } else {
+        if(index==0) {
+            index = totalImages - 1;
+        } else {
+            index--;
+        }
+    }
+    for(let i = 0; i <images.length; i++) {
+        images[i].classList.remove("main");
+    }
+    images[index].classList.add("main");
+}
+
+let signup = document.querySelector(".nav_signup");
+signup.addEventListener("click", myfun1);
+
+function myfun1(){
+    document.location = "signup.html";
+}
 // window.onscroll = function(){
 //     myfunction()
 // };
